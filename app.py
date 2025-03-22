9e0c9ed08a232c02ebd4aefdaebf7d1fca190687
kwaho5754 <kwanho20@gmail.com>
2025-03-23 00:02:14 +0900

    Fix: use PORT from environment for Render

--- app.py
+++ app.py
@@ -25,4 +25,6 @@ def predict():
     return jsonify({'result': prediction})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    from os import environ
+    port = int(environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
